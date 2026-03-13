5780a83fef91248f2082c4e11c4b8e0bf49eab23
AdrimesinKarl <jcarlo1002@gmail.com>
2026-03-13 16:28:33 +0800

    added python activities

---
+++ Activity1.py
@@ -0,0 +1,7 @@
+name = input ("What is your name? ")
+age = input ("What is your age? ")
+program = input ("What is your Program?")
+
+print ( "My name is " + name )
+print  ( "I am " + age + "years old" )
+print ( "I am studying " + program )
---
+++ Practice1.py
@@ -0,0 +1,16 @@
+name = "Pinocchio"
+age = 1  # He's a puppet newly brought to life!
+title = "The Living Puppet"
+race = "Wood / Enchanted"
+hometown = "Tuscany, Italy"
+biography = "Created by the woodcarver Geppetto in a small Italian village, " \
+"Pinocchio is a wooden puppet who dreams of becoming a real boy." \
+" He is known for his nose, which grows longer whenever he tells a lie," \
+" and is often guided by the Talking Cricket (Jiminy) on his journey" \
+" to learn about bravery, honesty, and unselfishness."
+
+print(name)
+print(str(age) + " year old")
+print(title)
+print(hometown)
+print(biography)
