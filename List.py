b7bd18244bc7b875635f994fb24aaa1d6bb08c19
AdrimesinKarl <jcarlo1002@gmail.com>
2026-03-25 10:57:25 +0800

    added a program that contains list

---
+++ List.py
@@ -0,0 +1,10 @@
+#this program contain a list of vegetables and will insert a new vegetable in the list and change the first vegetable to strawberry and then print all the vegetables in the list using a for loop.
+vegetable_basket = ["kalabansya", "malungGay","karrets"]
+vegetable_basket.insert(2,"eggplant")
+vegetable_basket[0] = "strawverry"
+
+for vegetable in vegetable_basket:
+    print(vegetable)
+
+
+
